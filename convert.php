2a1dd1b7350b6ba864b301644fb404252ea3a2ae
buzzy <daniele.testa@gmail.com>
2022-12-05 22:24:16 +0100

    Initial commit

--- convert.php
+++ convert.php
@@ -1,1 +1,28 @@
 <?php
+
+$spec = yaml_parse(file_get_contents('https://scalr.io/api/iacp/v3/openapi-preview.yml'));
+
+array_walk_recursive($spec, function(&$val, $key) {
+
+    if ($key == '$ref' && substr($val, 0, 9) == 'examples/') {
+        $val = 'https://scalr.io/api/iacp/v3/' . $val;
+    }
+
+});
+
+foreach ($spec['components']['schemas'] as $component => &$value) {
+
+    if (!isset($value['properties']['relationships']['properties'])) continue;
+
+    foreach ($value['properties']['relationships']['properties'] as $relationship => &$val) {
+        if (!isset($val['properties']['data']['properties']['type']['enum'])) continue;
+
+        array_unshift($val['properties']['data']['properties']['type']['enum'], '');
+    }    
+}
+
+$yaml = yaml_emit($spec);
+
+$yaml = str_replace('- items: []', '- items: {}', $yaml);
+
+file_put_contents('readme.yaml', $yaml);
